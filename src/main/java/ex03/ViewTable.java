c499b8a02780d8e3900f824b2a93dfca87953b36
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-22 20:11:10 +0200

    Implement ViewTable class with table display methods

--- src/main/java/ex03/ViewTable.java
+++ src/main/java/ex03/ViewTable.java
@@ -1,4 +1,43 @@
 package ex03;
 
-public class ViewTable {
+import ex02.ViewResult;
+import ex01.Item2d;
+
+public class ViewTable extends ViewResult {
+    private int width = 30;
+
+    public ViewTable() {
+        super();
+    }
+
+    public final void init(int width) {
+        this.width = width;
+        viewInit();
+    }
+
+    public void viewInit() {
+        System.out.println(" ");
+        super.viewInit();
+    }
+
+    private void line() {
+        for (int i = 0; i < width; i++) System.out.print("-");
+        System.out.println();
+    }
+
+    public void viewHeader() {
+        line();
+        System.out.printf("| %-10s | %-12s |\n", "Parametr", "Resultat");
+        line();
+    }
+
+    public void viewBody() {
+        for (Item2d it : getItems()) {
+            System.out.printf("| %-12s | %-30s |\n", "Energiia:", it.energy);
+        }
+    }
+
+    public void viewFooter() {
+        line();
+    }
 }
