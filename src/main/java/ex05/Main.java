0faf1ec2cd08a99d2bc8266268ea3bc0633e9159
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-22 20:04:04 +0200

    Update Main.java

--- src/main/java/ex05/Main.java
+++ src/main/java/ex05/Main.java
@@ -1,4 +1,33 @@
 package ex05;
 
+import ex02.View;
+import ex02.ViewableResult;
+import ex04.ChangeConsoleCommand;
+import ex04.GenerateConsoleCommand;
+import ex04.Menu;
+import ex04.ViewConsoleCommand;
+
 public class Main {
+    private View view = new ViewableResult().getView();
+
+    private Menu menu = new Menu();
+
+    public void run() {
+        menu.add(new ViewConsoleCommand(view));
+        menu.add(new GenerateConsoleCommand(view));
+        menu.add(new ChangeConsoleCommand(view));
+
+        menu.add(new ExecuteConsoleCommand(view));
+
+        try {
+            menu.execute();
+        } catch (Exception e) {
+            System.out.println("Pomylka: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
 }
