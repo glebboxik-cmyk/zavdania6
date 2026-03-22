6b6e759902b6160996baaa7f77cfdf9d2c88a3b7
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-22 20:13:49 +0200

    Update Item2d.java

--- src/main/java/ex01/Item2d.java
+++ src/main/java/ex01/Item2d.java
@@ -1,4 +1,30 @@
 package ex01;
 
-public class Item2d {
+import java.io.Serializable;
+
+public class Item2d implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public double m, v, h, energy;
+    public transient String bin;
+
+    public Item2d(double m, double v, double h, double e) {
+        this.m = m;
+        this.v = v;
+        this.h = h;
+        this.energy = e;
+        this.bin = Long.toBinaryString((long) e);
+    }
+
+    public double getY() {
+        return energy;
+    }
+
+    public double getX() {
+        return m;
+    }
+
+    public String toString() {
+        return "Energiia: " + energy + ", Dvikovo: " + bin;
+    }
 }
